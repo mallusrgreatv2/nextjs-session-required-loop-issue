67ed2f17d2e5834ccdc9bf824eeb3b1ea775270a
mallusrgreat <freedomgamingyt@gmail.com>
2024-01-12 17:48:29 +0530

    delete unused stuff

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.discordapp.com",
-        port: "",
-        pathname: "/avatars/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
